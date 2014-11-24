36d606975a0abbd23b634986ae2e1690e729ece5
Ben Stein <bumstain@gmail.com>
2014-11-24 16:35:27 +0000

    Established key classes and methods

--- prs_oop.rb
+++ prs_oop.rb
@@ -9,16 +9,46 @@ end
 
 class 
 	Player
-	
+	attr_accessor :player
+
+	def initialize(p)
+		@player = p
+	end
 end 
 
 class 
-	Computer
+	Opponent
+	attr_accessor :opponent
+
+	def initialize(o)
+		@opponent = o
+	end
 end
+
+class Prs
+	attr_accessor :player, :opponent, :value
+
+	def initialize
+		@player = Player.new("Ben")
+		@opponent = Opponent.new
+		@value = Choice.new 
+	end
+
+# 	def run 
+# 		player_choice
+# 		opponent_choice
+# 		compare_hands
+# 		who_won?
+# 	end
+
+end
+
+# Prs.new.run 
+
+
 
 
 hand = Choice.new('hand')
 
+puts "Welcome to Paper, Scissors, Rock! What's your name?"
 puts hand.value
-
-
