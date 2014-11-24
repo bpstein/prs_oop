a4e89385201384422a159337e3ed041f8ab5a14a
Ben Stein <bumstain@gmail.com>
2014-11-24 18:38:29 +0000

    Basic gameplay engine functions correctly

--- prs_oop.rb
+++ prs_oop.rb
@@ -1,54 +1,74 @@
-class 
-	Choice
-	attr_accessor :value
+class Player
+	attr_accessor :choice
+	attr_reader :name
+	def initialize(n)
+		@name = n
+	end
 
-	def initialize(v)
-		@value = ['p', 'r', 's']
+	def make_choice
+		begin 
+    	puts "Choose either paper(p), rock(r) or scissors(s)."
+    	c = gets.chomp.downcase
+    end until Game::CHOICES.keys.include?(c)
+
+    self.choice = c
+	end
+	def player_choice
+		"#{name} chose #{choice}!"
 	end
 end 
 
-class 
-	Player
-	attr_accessor :player
 
-	def initialize(p)
-		@player = p
-	end
-end 
 
-class 
-	Opponent
-	attr_accessor :opponent
+
+
+class Opponent
+	attr_accessor :choice
+	attr_reader :name
 
 	def initialize(o)
 		@opponent = o
 	end
+
+	def random_choice
+		c = Game::CHOICES.keys.sample
+		self.choice = c
+	end
+
+	def computer_choice
+		"#{name} chose #{choice}!"
+	end
 end
 
-class Prs
-	attr_accessor :player, :opponent, :value
+
+
+
+class Game
+	CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
+	attr_reader :player, :opponent
 
 	def initialize
 		@player = Player.new("Ben")
-		@opponent = Opponent.new
-		@value = Choice.new 
+		@opponent = Opponent.new("Computer")
 	end
 
-# 	def run 
-# 		player_choice
-# 		opponent_choice
-# 		compare_hands
-# 		who_won?
-# 	end
-
+	def play
+		player.make_choice
+		opponent.random_choice
+		puts player.player_choice
+		puts opponent.computer_choice
+	end
 end
 
-# Prs.new.run 
+
+
+game = Game.new.play
 
 
 
 
-hand = Choice.new('hand')
 
-puts "Welcome to Paper, Scissors, Rock! What's your name?"
-puts hand.value
+# puts "Let's play Paper, Scissors, Rock! What's your name?"
+# player = gets.chomp
+# puts "Welcome #{player}, choose either paper (p), rock (r) or scissors(s)."
+# puts hand.value
